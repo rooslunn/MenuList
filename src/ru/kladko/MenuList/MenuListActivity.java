44c270dd4f16166ed770ec66846c06f0bcb52082
Rooslunn <rooslunn@gmail.com>
2013-03-06 12:44:40 +0200

    Initial commit

---
+++ src/ru/kladko/MenuList/MenuListActivity.java
@@ -0,0 +1,15 @@
+package ru.kladko.MenuList;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuListActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
